2792b798960b1b1156ae3e40a7e999c9e242b3a9
Alex <61283644+KafaloAlex@users.noreply.github.com>
2021-01-04 06:22:39 +0000

    Add program

---
+++ PGCD Egyptien/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace PGCD_Egyptien
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Title = "Calcul du pgcd (Méthode Egyptienne)";
+
+            Console.Write("Entrez le premier nombre : ");
+            var n1 = Int32.Parse(Console.ReadLine());
+
+            Console.Write("Entrez le premier nombre : ");
+            var n2 = Int32.Parse(Console.ReadLine());
+
+            Console.WriteLine();
+
+            if (n1 * n2 != 0)
+                Console.WriteLine("pgcd({0};{1}) = {2}", n1, n2, pgcd(n1, n2));
+            else
+                Console.WriteLine("le pgcd de ces nombres n'existent pas");
+
+        }
+
+        static int pgcd (int x, int y)
+        {
+            while (x != y)
+            {
+                if (x > y)
+                    x -= y;
+                else
+                    y -= x;
+            }
+            return x;
+        }
+    }
+}
